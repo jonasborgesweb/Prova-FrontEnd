cfabeed2c8b76ac8aac6c440fb14eba850a2d85f
jonasborgesweb <jonasborgesweb@gmail.com>
2020-01-04 20:48:46 -0300

    Adicionado as Informações do Card na Tela

--- prova-tecnica/src/App.js
+++ prova-tecnica/src/App.js
@@ -1,22 +1,31 @@
 import React, {Component} from 'react';
 import './App.css';
 
+//Importando o Card
+import Card from './components/card'
+
 //Importando a Lista
 import ListData from './data/lista.json'
 
 class App extends Component{
 
+  constructor(props) {
+    super(props)
+    this.state = this.getInitialState()
+  }
+
+  getInitialState() {
+    return {
+      ListData: ListData      
+    }
+  }
+
 
   render(){
+    let itens = this.state.ListData;
     return(
       <div>
-        {ListData.map((item,index)=> {
-          return <div>
-            <p>{item.id}</p>
-            <p>{item.nome}</p>
-            <p>{item.email}</p>
-          </div>
-        })}
+        {itens.map((item, index) => <Card key={index} card={item} />)}
       </div>
     );
   }
--- prova-tecnica/src/components/card/index.jsx
+++ prova-tecnica/src/components/card/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import './card.css'
 
-//Importando os Icones
 import iconUsername from '../../icons/user.svg'
 import iconEmail from '../../icons/email.svg'
 import iconAddress from '../../icons/address.svg'
@@ -10,37 +9,39 @@ import iconPhone from '../../icons/phone.svg'
 
 const Card = (props) => {
 
-    const {card} = props;
+    const { card } = props;
 
-    return(
+    return (
         <div className="card">
             <div className="card__content">
                 <div className="card__row">
-                    <p className="card__texto--nome">Nome</p>
+                    <p className="card__texto--nome">{card.name}</p>
                 </div>
                 <div className="card__row">
-                    <img src="" alt="Username" className="card__icon"/>
-                    <p className="card__texto">UserName</p>
+                    <img className="card__icon" src={iconUsername} alt="Username" title="Username"/>
+                    <p className="card__texto" title="Username">{card.username}</p>
                 </div>
                 <div className="card__row">
-                    <img src="" alt="email" className="card__icon"/>
-                    <p className="card__texto">email</p>
+                    <img className="card__icon" src={iconEmail} alt="E-mail" title="E-mail"/>
+                    <p className="card__texto" title="E-mail">{card.email}</p>
                 </div>
                 <div className="card__row">
-                    <img src="" alt="phone" className="card__icon"/>
-                    <p className="card__texto">phone</p>
+                    <img className="card__icon" src={iconPhone} alt="Phone" title="Phone"/>
+                    <p className="card__texto" title="Phone">{card.phone}</p>
                 </div>
                 <div className="card__row">
-                    <img src="" alt="website" className="card__icon"/>
-                    <p className="card__texto">website</p>
+                    <img className="card__icon" src={iconSite} alt="Website" title="Website"/>
+                    <p className="card__texto" title="Website">{card.website}</p>
                 </div>
                 <div className="card__row">
-                    <img src="" alt="address" className="card__icon"/>
-                    <p className="card__texto">address</p>
+                    <img className="card__icon" src={iconAddress} alt="Address" title="Address"/>
+                    <p className="card__texto" title="Address">
+                        {card.address.street + ', ' + card.address.suite + ', ' + card.address.city}
+                    </p>
                 </div>
             </div>
         </div>
-    );
+    )
 }
 
 export default Card
