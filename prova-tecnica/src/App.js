ce877b489fa6022e52178e58437d292197973a27
jonasborgesweb <jonasborgesweb@gmail.com>
2020-01-04 20:25:21 -0300

    Adicionado o Arquivo Json e Recuperado os valores na tela

--- prova-tecnica/src/App.js
+++ prova-tecnica/src/App.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div>
-      <h1>Hello Word</h1>
-    </div>
-  );
+//Importando a Lista
+import ListData from './data/lista.json'
+
+class App extends Component{
+
+
+  render(){
+    return(
+      <div>
+        {ListData.map((item,index)=> {
+          return <div>
+            <p>{item.id}</p>
+            <p>{item.nome}</p>
+            <p>{item.email}</p>
+          </div>
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
