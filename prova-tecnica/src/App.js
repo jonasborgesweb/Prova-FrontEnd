9804c71414b13ac6ddb1a36d4ceacc8f9cab2b8d
jonasborgesweb <jonasborgesweb@gmail.com>
2020-01-05 13:11:05 -0300

    Adicionado os Filtros na Página e Removido arquivos desnecessários

--- prova-tecnica/src/App.js
+++ prova-tecnica/src/App.js
@@ -20,6 +20,29 @@ class App extends Component{
     }
   }
 
+  getFilterOptions() {
+    let emails = [];
+
+    emails = ListData.map(item => item.email)
+
+    const options = emails.map(email => {
+      const result = email.split('.')
+      return result[result.length - 1]
+    })
+    return [...new Set(options)]
+  }
+
+  changeFilter(event) {  
+    const filter = event.target.value  
+    if (event ==='')
+      return     
+
+    const itens = ListData.filter(item => item.email.includes(filter))
+    this.setState({
+      ListData: itens    
+    })
+  }
+
 
   render(){
     let itens = this.state.ListData;
@@ -27,11 +50,28 @@ class App extends Component{
       <section className="list">
         <div className="center">
           <div className="search">
-            <p className="search__title">Aqui vai a Pesquisa</p>
+            <h1 className="search__title">Pesquisa</h1>
+            <p className="search__description">Selecione o host do email para realizar a filtragem</p>
+            <select className="search__select" onChange={(event) => this.changeFilter(event)}>
+              <option className="search__option" value="">All</option>
+              {this.getFilterOptions().map((item, index) => <option className="search__option" key={index} value={item}>{item}</option>)}
+            </select>
           </div>
           <div className="list__wrapper">
             <aside className="sidebar">
-              <p>Aqui é a Sidebar</p>
+              <h2 className="sidebar__title">Acesso Rápido</h2>
+              <ul className="sidebar__lista">
+                <li className="sidebar__item">
+                  <input className="sidebar__radio" value="" type="radio" id="all" name="sidebar" onChange={(event) => this.changeFilter(event)}/>
+                  <label for="all" className="sidebar__label">All</label>
+                </li>
+                {this.getFilterOptions().map((item, index) => 
+                  <li className="sidebar__item" key={index} >
+                    <input className="sidebar__radio" type="radio" key={index} value={item} id={item} name="sidebar" onChange={(event) => this.changeFilter(event)}/>
+                    <label for={item} className="sidebar__label">{item}</label>
+                  </li>
+                )}
+              </ul>
             </aside>
             <div className="list__content">
               {itens.map((item, index) => <Card key={index} card={item} />)}
